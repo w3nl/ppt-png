b00b026898a2fc0af31042c09c3e5a8972b21d06
Pieter Wigboldus <pieter@way2web.nl>
2017-09-07 11:50:43 +0200

    Add real mocha test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,48 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var Converter = require('../js/convert.js');
+
+describe('ppt-png', function() {
+    describe('normal', function() {
+        it('should save without error', function() {
+            new Converter({
+                files:          ['test/OPW 733 Tienduizend redenen.ppt'],
+                output:         'output/test/',
+                invert:         true,
+                greyscale:      true,
+                deletePdfFile:  false,
+                outputType:     'jpg',
+                logLevel:       3,
+                fileNameFormat: '_vers_%d',
+                callback:       function(data) {
+                    console.log(data.failed, data.success.length, data.files.length, data.time);
+                }
+            }).run();
+        });
     });
-  });
+
+    describe('promise', function() {
+        it('should save without error', function() {
+            new Converter({
+                files:          ['test/OPW 733 Tienduizend redenen.ppt'],
+                output:         'output/test/',
+                invert:         true,
+                greyscale:      true,
+                deletePdfFile:  true,
+                outputType:     'png',
+                logLevel:       2,
+                fileNameFormat: '_vers_%d'
+            })
+                .wait()
+                .then(function(data) {
+                    if(data.failed.length > 0) {
+                        done(data.failed);
+                    } else {
+                        done();
+                    }
+                })
+                .catch(function(error) {
+                    done(error);
+                });
+        });
+    });
 });
-
-
