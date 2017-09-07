3bfcd5f6eac1229fed1acb4d48a0301427c2ce70
Pieter Wigboldus <pieter@way2web.nl>
2017-09-07 11:20:00 +0200

    Add a mocha test

---
+++ example/test.js
@@ -0,0 +1,22 @@
+var Converter = require('../js/convert.js');
+var glob = require('glob');
+
+// options is optional
+glob('input/test/*.ppt', {}, function(er, files) {
+    console.log('files: ', files.length);
+    if(files) {
+        new Converter({
+            files:          files,
+            output:         'output/test/',
+            invert:         true,
+            greyscale:      true,
+            deletePdfFile:  false,
+            outputType:     'png',
+            logLevel:       2,
+            fileNameFormat: '_vers_%d',
+            callback:       function(data) {
+                console.log(data.failed, data.success.length, data.files.length, data.time);
+            }
+        }).run();
+    }
+});
--- test/test.js
+++ test/test.js
@@ -1,22 +1,10 @@
-var Converter = require('../js/convert.js');
-var glob = require('glob');
-
-// options is optional
-glob('input/test/*.ppt', {}, function(er, files) {
-    console.log('files: ', files.length);
-    if(files) {
-        new Converter({
-            files:          files,
-            output:         'output/test/',
-            invert:         true,
-            greyscale:      true,
-            deletePdfFile:  false,
-            outputType:     'png',
-            logLevel:       2,
-            fileNameFormat: '_vers_%d',
-            callback:       function(data) {
-                console.log(data.failed, data.success.length, data.files.length, data.time);
-            }
-        }).run();
-    }
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
 });
+
+
