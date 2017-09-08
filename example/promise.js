1e4aee94d7ef57b742fe74b770c6e89a90fddb57
Pieter Wigboldus <w3n@w3news.org>
2017-09-08 09:27:06 +0200

    Try again failed files

--- example/promise.js
+++ example/promise.js
@@ -1,8 +1,18 @@
 var Converter = require('../js/convert.js');
 var glob = require('glob');
 
+require('array-helpers');
+
 // options is optional
-glob('input/test/*.ppt', {}, function(error, files) {
+glob('input/test/*.ppt', {}, convert);
+
+/**
+ * Convert the files.
+ *
+ * @param {object} error
+ * @param {array} files
+ */
+function convert(error, files) {
     console.log('files: ', files.length);
     if(files) {
         new Converter({
@@ -16,6 +26,9 @@ glob('input/test/*.ppt', {}, function(error, files) {
             fileNameFormat: '_vers_%d'
         }).wait().then(function(data) {
             console.log(data.failed, data.success.length, data.files.length, data.time);
+            if(data.failed.length > 0) {
+                convert(null, data.failed.multikey('file'));
+            }
         });
     }
-});
+}
