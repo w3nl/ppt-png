924903ae39c4ffae8c2587db28a6a2f159637b9c
Pieter Wigboldus <w3n@w3news.org>
2021-01-06 08:58:55 +0100

    Cleanup the codebase

---
+++ src/pdf2png.js
@@ -0,0 +1,12 @@
+import Pdf2PngConverter from '@hckrnews/pdf2png';
+
+export default ({
+    file, output
+}) => {
+    const pdfConverter = Pdf2PngConverter.create({
+        file,
+        output
+    });
+
+    return pdfConverter.convert();
+};
---
+++ src/ppt2pdf.js
@@ -0,0 +1,14 @@
+import PptToPdfConverter from '@hckrnews/ppt2pdf';
+
+export default ({
+    file, output
+}) => {
+    const pptConverter = PptToPdfConverter.create({
+        file: file.path,
+        output
+    });
+
+    pptConverter.convert();
+
+    return pptConverter.pdf;
+};
