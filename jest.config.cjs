6cb0684b7913095cc05f3513209c3e646c3ebeda
Pieter Wigboldus <w3n@w3news.org>
2022-03-03 16:24:24 +0100

    Update dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,19 +2,19 @@ module.exports = {
     moduleFileExtensions: ['js', 'jsx', 'json'],
 
     transform: {
-        '^.+\\.js?$': 'babel-jest',
+        '^.+\\.js?$': 'babel-jest'
     },
 
     transformIgnorePatterns: ['/node_modules/'],
 
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@/(.*)$': '<rootDir>/src/$1'
     },
 
     testMatch: ['**/__tests__/*.js'],
 
     testURL: 'http://localhost/',
 
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js'],
+    collectCoverage:     true,
+    collectCoverageFrom: ['src/**/*.js']
 };
